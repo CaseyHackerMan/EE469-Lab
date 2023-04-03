7f115fa0942c53fe7fcc78c0edf1fd568238138d
Casey <caseyculbertson1@gmail.com>
2023-04-03 13:33:42 -0700

    made testbench file

--- reg_file.sv
+++ reg_file.sv
@@ -19,21 +19,5 @@ module reg_file(input logic clk, wr_en,
 endmodule
 
 
-module reg_file_testbench();
 
-	logic clk, wr_en;
-	logic [31:0] write_data, read_data1, read_data2;
-	logic [3:0] write_addr, read_addr1, read_addr2;
-	
-	reg_file dut (clk, wr_en, write_data, write_addr, read_addr1, read_addr2, read_data1, read_data2);
-	initial begin
-	
-		
-		
-		$stop;
-		
-	end
-	
-endmodule
-			
 		
