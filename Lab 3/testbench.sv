8439c610eba7d5b4f262d8fef8c483070d170d0e
clowdur <89669586+clowdur@users.noreply.github.com>
2023-05-05 05:45:44 +0000

    task 3 test in testbench & imem read memfile3

--- Lab 3/testbench.sv
+++ Lab 3/testbench.sv
@@ -32,9 +32,12 @@ module testbench();
         // else                                                 $display("Task 1 Failed");
 
         // task 2:
-        assert(cpu.processor.u_reg_file.memory[8] == 32'd1)  $display("Task 2 Passed");
-        else                                                 $display("Task 2 Failed");
+        // assert(cpu.processor.u_reg_file.memory[8] == 32'd1)  $display("Task 2 Passed");
+        // else                                                 $display("Task 2 Failed");
 
+		// task 3:
+		assert(cpu.processor.u_reg_file.memory[2] == 32'd0)  $display("Task 2 Passed");
+        else                                                 $display("Task 2 Failed");
         $stop;
     end
 
